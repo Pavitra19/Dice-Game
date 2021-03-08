aa8f722f1a30077382f9701b8660907e934355d8
Pavitra Kurseja <pavitra.kurseja@gmail.com>
2021-03-07 22:49:01 -0500

    Clean up code

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; // Generate a random # between 1 and 6
-document
+var randomDiceImageSource1 = "images/dice" + randomNumber1 + ".png";
+var img1 = document
   .querySelector(".img1")
-  .setAttribute("src", "images/dice" + randomNumber1 + ".png"); // change dice img to match random number
+  .setAttribute("src", randomDiceImageSource1); // change dice img to match random number
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1; // Generate a second random # between 1 and 6
-document
+var randomDiceImageSource2 = "images/dice" + randomNumber2 + ".png";
+var img2 = document
   .querySelector(".img2")
-  .setAttribute("src", "images/dice" + randomNumber2 + ".png");
+  .setAttribute("src", randomDiceImageSource2);
 
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "Player 1 Wins!"; // if player 1 has a larger value, they win
+  document.querySelector("h1").innerHTML = "💯 Player 1 Wins!"; // if player 1 has a larger value, they win
 } else if (randomNumber1 < randomNumber2) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins!"; // if player 2 has a larger value, they win
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 💯"; // if player 2 has a larger value, they win
 } else {
   document.querySelector("h1").innerHTML = "Draw!"; // if player 1 and 2 have the same value, it's a draw
 }
