4630b1fe092828217b92dde7879804bd8e2dcb3e
Pavitra Kurseja <pavitra.kurseja@gmail.com>
2021-03-07 22:20:45 -0500

    Use random numbber to pick out random dice image for first dice

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; // Generate a random # between 1 and 6
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
