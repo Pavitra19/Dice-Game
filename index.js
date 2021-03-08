81a7f50ee4a76c1114c37a0a0e6a25f818524193
Pavitra Kurseja <pavitra.kurseja@gmail.com>
2021-03-07 22:25:39 -0500

    Change image of second dice based on random number 2

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; // Generate a random # between 1 and 6
-document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+document
+  .querySelector(".img1")
+  .setAttribute("src", "images/dice" + randomNumber1 + ".png"); // change dice img to match random number
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1; // Generate a second random # between 1 and 6
+document
+  .querySelector(".img2")
+  .setAttribute("src", "images/dice" + randomNumber2 + ".png");
